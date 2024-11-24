6bc418455576586a9f9f3715909fa49a713d3b2a
Ashish Rolan <124621644+ashish7472@users.noreply.github.com>
2024-11-24 18:08:44 +0530

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -30,7 +30,7 @@ const LandingPage = () => {
   useEffect(() => {
     const fetchUsersWithSimilarInterests = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/getUsersWithSimilarInterests', {
+        const response = await axios.get('https://campus-connect-backend-bugc.onrender.com/getUsersWithSimilarInterests', {
           params: { email: userEmail },
         });
 
